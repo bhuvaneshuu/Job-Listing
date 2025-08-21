d7691211d6fb6ccfba10be7486e789a79dcf1fd8
bhuvaneshuu <bhuvaneshaym@gmail.com>
2025-08-21 12:03:40 +0530

    update1

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,16 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     port: 5173,
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'https://job-listing-q0po.onrender.com', // 👈 Render backend
         changeOrigin: true,
+        secure: false,
       },
     },
   },
 })
+
